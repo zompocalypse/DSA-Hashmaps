334233f7ecd20d860b545b8050e6f1e8f6708c15
Jason Stankevich <jason.stankevich@gmail.com>
2020-11-03 09:11:47 -0500

    complete

--- HashMapChaining.js
+++ HashMapChaining.js
@@ -1,4 +1,4 @@
-class HashMap {
+class HashMapChain {
   constructor(initialCapacity = 8) {
     this.length = 0;
     this._hashTable = [];
@@ -6,32 +6,80 @@ class HashMap {
     this._deleted = 0;
   }
 
+  display() {
+    this._hashTable.forEach((item) => {
+      if (item) {
+        console.log(item);
+        while (item.next) {
+          console.log(item.next);
+          item = item.next;
+        }
+      }
+    });
+  }
+
   get(key) {
     const index = this._findSlot(key);
+
+    let currItem = this._hashTable[index];
+
     if (this._hashTable[index] === undefined) {
       throw new Error('Key error');
     }
-    return this._hashTable[index].value;
+
+    while (currItem.key !== key) {
+      currItem = currItem.next;
+    }
+
+    return currItem.value;
   }
 
   set(key, value) {
     const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-      this._resize(this._capacity * HashMap.SIZE_RATIO);
+
+    if (loadRatio > HashMapChain.MAX_LOAD_RATIO) {
+      this._resize(this._capacity * HashMapChain.SIZE_RATIO);
     }
-    //Find the slot where this key should be in
     const index = this._findSlot(key);
 
-    if (!this._hashTable[index]) {
-      this._hashTable[index] = [];
+    let currItem = this._hashTable[index];
+
+    if (!currItem) {
+      this.length++;
+
+      this._hashTable[index] = {
+        key,
+        value,
+        next: null,
+        DELETED: false,
+      };
+    } else {
+      while (currItem.key !== key) {
+        if (currItem.next === null) {
+          currItem.next = {
+            key,
+            value,
+            next: null,
+            DELETED: false,
+          };
+          this.length++;
+          return;
+        }
+      }
+
+      currItem.value = value;
     }
-    this._hashTable[index].push([key, value]);
   }
 
   delete(key) {
     const index = this._findSlot(key);
-    const slot = this._hashTable[index];
-    if (slot === undefined) {
+    let slot = this._hashTable[index];
+
+    while (slot.key !== key && slot) {
+      slot = slot.next;
+    }
+
+    if (!slot) {
       throw new Error('Key error');
     }
     slot.DELETED = true;
@@ -40,29 +88,34 @@ class HashMap {
   }
 
   _findSlot(key) {
-    const hash = HashMap._hashString(key);
+    const hash = HashMapChain._hashString(key);
     const start = hash % this._capacity;
 
     for (let i = start; i < start + this._capacity; i++) {
       const index = i % this._capacity;
+
       const slot = this._hashTable[index];
       if (slot === undefined || (slot.key === key && !slot.DELETED)) {
         return index;
       }
+
+      return index;
     }
   }
 
   _resize(size) {
     const oldSlots = this._hashTable;
     this._capacity = size;
-    // Reset the length - it will get rebuilt as you add the items back
     this.length = 0;
     this._deleted = 0;
     this._hashTable = [];
 
-    for (const slot of oldSlots) {
+    for (let slot of oldSlots) {
       if (slot !== undefined && !slot.DELETED) {
-        this.set(slot.key, slot.value);
+        while (slot !== null) {
+          this.set(slot.key, slot.value);
+          slot = slot.next;
+        }
       }
     }
   }
@@ -70,17 +123,9 @@ class HashMap {
   static _hashString(string) {
     let hash = 5381;
     for (let i = 0; i < string.length; i++) {
-      //Bitwise left shift with 5 0s - this would be similar to
-      //hash*31, 31 being the decent prime number
-      //but bit shifting is a faster way to do this
-      //tradeoff is understandability
       hash = (hash << 5) + hash + string.charCodeAt(i);
-      //converting hash to a 32 bit integer
       hash = hash & hash;
     }
-    //making sure hash is unsigned - meaning non-negtive number.
     return hash >>> 0;
   }
 }
-
-module.exports = HashMap;
